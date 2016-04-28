d33593b4404aa01bf69cd2b90f8f91814513c1ef
Christopher Roussel <christopher@impleri.net>
2016-04-28 15:54:57 -0500

    Adding Atom, plus a few minor changes

--- zsh/plugins/personal/personal.plugin.zsh
+++ zsh/plugins/personal/personal.plugin.zsh
@@ -1,7 +1,7 @@
 # Adds other aliases
 
 alias bfg='java -jar bfg.jar'
-alias composer='hhvm ~/.bin/composer.phar'
+alias composer='hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 ~/.bin/composer.phar'
 alias history='fc -f -l 1'
 alias hs='homestead'
 alias texclean='rm *.backup *.run.xml *.log *.aux *-blx.bib *.dvi'
